edc4c0e71e43bd6c22d9b2e1449032987270759b
animeshshukla06 <64635584+animeshshukla06@users.noreply.github.com>
2021-03-23 14:17:34 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request
 import numpy as np
 import pickle
-import mysql.connector
+# import mysql.connector
 
 # store the user's data in the database:
 # def store_data(form_data_dict, pred_class):
